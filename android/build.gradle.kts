4450fa2da80b0c46589d326548785f6f2a816c5a
Yi Ding <yi.s.ding@gmail.com>
2025-09-05 23:08:03 -0700

    gradlew

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("org.jmailen.kotlinter") version "5.2.0"
 }
 
 buildscript {
